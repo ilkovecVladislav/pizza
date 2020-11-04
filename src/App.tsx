5f0eb7300fe63a2fc06048c29213a91fc5902d78
Vladislav Ilkovec <ilkovec.vi@gmail.com>
2020-11-04 19:50:18 +0300

    chore: add conventional commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 interface AppProps {}
 
@@ -16,7 +14,6 @@ function App({}: AppProps) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
