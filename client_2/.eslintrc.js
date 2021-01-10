53cb49530e6c94816b6c79e1090a39bd7ca800f5
Uladzislau Ilkavets <ilkovec.vi@gmail.com>
2021-01-10 14:45:23 +0300

    PIZZA-refactoring
    Added cra for client part

---
+++ client_2/.eslintrc.js
@@ -0,0 +1,87 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:sonarjs/recommended',
+  ],
+  plugins: ['react', 'react-hooks', 'jsx-a11y', '@typescript-eslint', 'prettier', 'sonarjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    'padding-line-between-statements': ['warn', { blankLine: 'always', prev: '*', next: 'return' }],
+    'arrow-body-style': 'warn',
+    'react/require-default-props': 'off',
+    'require-await': 'off',
+    'react/display-name': 'off',
+    '@typescript-eslint/require-await': 'off',
+    'linebreak-style': 'off',
+    'no-param-reassign': 'off',
+    '@typescript-eslint/unbound-method': 'off',
+    'prettier/prettier': ['error'],
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'jsx-a11y/control-has-associated-label': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    'max-len': [
+      'warn',
+      {
+        code: 100,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+    'lines-between-class-members': [
+      'error',
+      'always',
+      {
+        exceptAfterSingleLine: true,
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['**/*.test.ts', '**/*.test.tsx', '**/setupTests.ts'] },
+    ],
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+  },
+  overrides: [
+    {
+      files: ['**/*.tsx'],
+      rules: {
+        'react/prop-types': 'off',
+      },
+    },
+  ],
+};
